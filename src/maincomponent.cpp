217a73ed5e4d4400e78bba1ef2b863eb3bc0b1c7
Richard Schwalk <richard.schwalk@gmail.com>
2015-10-15 08:27:54 +0200

    Simple working main component

--- src/maincomponent.cpp
+++ src/maincomponent.cpp
@@ -1,43 +1,71 @@
 #include <iostream>
 #include "maincomponent.h"
 
-MainComponent::MainComponent()
+MainComponent::MainComponent() :
+    m_isRunning{false}
 {
     m_window = new Window{WIDTH, HEIGHT, TITLE};
 
     m_window->makeContextCurrent();
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+
 }
 
 void MainComponent::start()
 {
-    run();
+    if (!m_isRunning) {
+        run();
+    }
 }
 
 void MainComponent::stop()
 {
+    if(m_isRunning) {
+        m_isRunning = false;
+    }
 
 }
 
 void MainComponent::run()
 {
-    while (!m_window->shouldClose()) {
+    m_isRunning = true;
+
+    while (m_isRunning) {
+        if (m_window->shouldClose()) {
+            stop();
+        }
+
+        // maybee all this transfer to the m_window render() methode
         glViewport(0, 0, m_window->getFrameBufferWidth(), m_window->getFrameBufferHeight());
         glClear(GL_COLOR_BUFFER_BIT);
+
+        render();
 
         glfwPollEvents();
         m_window->swapBuffers();
     }
+
+    cleanUp();
 }
 
 void MainComponent::render()
 {
-
+    m_window->render();
 }
 
 void MainComponent::cleanUp()
 {
 
+}
+
+int MainComponent::getWidth()
+{
+    return m_window->getFrameBufferWidth();
+}
+
+int MainComponent::getHeight()
+{
+    return m_window->getFrameBufferHeight();
 }
 
--- src/maincomponent.h
+++ src/maincomponent.h
@@ -9,15 +9,20 @@ public:
     MainComponent();
     void start();
     void stop();
-    void run();
-    void render();
-    void cleanUp();
 
 private:
     static constexpr int WIDTH{800};
     static constexpr int HEIGHT{600};
     const std::string TITLE{"3D Engine"};
     Window* m_window;
+    bool m_isRunning;
+
+    void run();
+    void render();
+    void cleanUp();
+    int getWidth();
+    int getHeight();
+    std::string getTitle() { return TITLE; }
 };
 
 #endif // MAINCOMPONENT_H
